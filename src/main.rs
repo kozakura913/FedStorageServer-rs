8592d261eacaf24d5fc6a4713006f575c16ca32c
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2025-02-15 22:55:07 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
 	//let bind=std::net::TcpListener::bind("0.0.0.0:3030");
 }
-fn wip_fn()->i32{
+fn wip_fn() -> i32 {
 	9
 }
 #[test]
-fn wip(){
-	assert_eq!(9,wip_fn());
+fn wip() {
+	assert_eq!(9, wip_fn());
 }
