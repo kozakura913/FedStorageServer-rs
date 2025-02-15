735b605cf42c299df30428e3f3ea04a561db7b7c
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2025-02-15 22:51:09 +0900

    space

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-	//let bind=std::net::TcpListener::bind("0.0.0.0:3030");
+    //let bind=std::net::TcpListener::bind("0.0.0.0:3030");
 }
 fn wip_fn()->i32{
-	9
+    9
 }
 #[test]
 fn wip(){
